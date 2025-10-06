d30d6eee845cb289156447e310776ff14b3d0f6b
bipin7745 <tanytask1919@gmail.com>
2025-10-06 14:42:27 +0530

    data2 file code loading fetchdata code chenge

--- react_components/src/componets/Data2.jsx
+++ react_components/src/componets/Data2.jsx
@@ -1,32 +1,54 @@
-import MyCard from './card2';
 import { useState, useEffect } from "react";
+import MyCard from './card2';
+import React, { Suspense } from "react";
 
 function FetchDataAPI1() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-      const data = await response.json();
-      setPosts(data);
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false); 
+      }
     }
     fetchData();
   }, []);
 
-  return (  
+  return (
     <div className="container py-4">
       <h1 className="mb-4 text-center">API Data Cards</h1>
-      <div className="row">
-        {posts.map((post) => (
-          <div className="col-md-4 mb-4" key={post.id}>
-            <MyCard
-              id={post.id}
-              title={post.title}
-              body={post.body}
-            />
+      
+      {loading ? (
+        <div className="text-center ">
+          <div className="spinner-border text-primary" style={{ width: "100px", height: "100px", marginTop:"200px"}} role="status">
+            <span className="visually-hidden">Loading...</span>
           </div>
-        ))}
-      </div>
+          <p>Loading posts...</p>
+        </div>
+      ) : (
+        <div className="row">
+            <Suspense fallback={<div className="">Loading...</div>}>
+          {posts.map((post) => (
+            <div className="col-md-4 mb-4" key={post.id}>
+              <MyCard
+                id={post.id}
+                title={post.title}
+                body={post.body}
+              />
+            </div>
+          )) }
+          </Suspense>
+        </div>
+        
+      )}
     </div>
   );
 }
